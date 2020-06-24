71e05e034f33eb981a3939ec290a680d8c22d83c
ChoKyuWon <yyj10137@gmail.com>
2020-06-24 14:57:38 +0900

    push_back

--- week0x00/chokyuwon/src/main.rs
+++ week0x00/chokyuwon/src/main.rs
@@ -1,34 +1,40 @@
-enum NextNode{
-    Node(Box<LinkedList>),
-    Nil,
-}
-impl NextNode{
-    fn node(&self){
-    }
-}
+// enum NextNode{
+//     Node(Box<LinkedList>),
+//     Nil,
+// }
+// impl NextNode{
+//     fn node(&self){
+//         /*
+//         match(self){
+//             Nil => 0,
+//             Node => Node.Box::<LinkedList>.data,
+//         }*/
+//     }
+// }
 
 struct LinkedList{
     data:u32,
-    //next:Box<LinkedList>,
-    next: NextNode
+    next: Option<Box<LinkedList>>
 }
 
 impl LinkedList{
-    fn push_back(&self, data:u32){}
-    fn push_arbitary(&self, data:u32, position:u32){}
-    fn read(&self, position:u32){}
-    fn del(&self, position:u32){}
+    fn push_back(&mut self, _data:u32){
+        let newnode = LinkedList{
+            data: _data,
+            next: None,
+        };
+        self.next = Option::Some(Box::<LinkedList>::new(newnode));
+    }
+    // fn push_arbitary(&self, data:u32, position:u32){}
+    // fn read(&self, position:u32){}
+    // fn del(&self, position:u32){}
 }
 
 fn main() {
-    let newnode = LinkedList{
-        data: 10,
-        next: NextNode::Nil,
-    };
-    let head = LinkedList{
+    let mut head = LinkedList{
         data: 8,
-        //next: NextNode::Node(Box::<LinkedList>::new(newnode)),
-        next: NextNode::Nil,
+        next: None,
     };
-    println!("{}, {}", head.data, newnode.data);
+    head.push_back(10);
+    println!("{}, {}", head.data, head.next.unwrap().data);
 }
