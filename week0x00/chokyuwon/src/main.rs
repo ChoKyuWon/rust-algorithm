d7c6126a9a5df113fbd2ba20e75934ecfe836c48
ChoKyuWon <yyj10137@gmail.com>
2020-06-24 13:29:01 +0900

    lalalala

--- week0x00/chokyuwon/src/main.rs
+++ week0x00/chokyuwon/src/main.rs
@@ -1,8 +1,34 @@
-struct LinkedList<'a>{
-    data : u32,
-    next : &'a LinkedList
+enum NextNode{
+    Node(Box<LinkedList>),
+    Nil,
+}
+impl NextNode{
+    fn node(&self){
+    }
+}
+
+struct LinkedList{
+    data:u32,
+    //next:Box<LinkedList>,
+    next: NextNode
+}
+
+impl LinkedList{
+    fn push_back(&self, data:u32){}
+    fn push_arbitary(&self, data:u32, position:u32){}
+    fn read(&self, position:u32){}
+    fn del(&self, position:u32){}
 }
 
 fn main() {
-    println!("Hello, world! kyuwon");
+    let newnode = LinkedList{
+        data: 10,
+        next: NextNode::Nil,
+    };
+    let head = LinkedList{
+        data: 8,
+        //next: NextNode::Node(Box::<LinkedList>::new(newnode)),
+        next: NextNode::Nil,
+    };
+    println!("{}, {}", head.data, newnode.data);
 }
